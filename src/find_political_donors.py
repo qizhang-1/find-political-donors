4a1b40aae9d56db7771649864cc10949c67812d3
qizhang-1 <bl1008@gmail.com>
2017-10-25 20:17:34 -0700

    1st workding condition

---
+++ src/find_political_donors.py
@@ -0,0 +1,15 @@
+import sys
+import ReadFile as rf
+
+
+def main():
+    input_filename = sys.argv[1]
+    output_filename_id_zip = sys.argv[2]
+    output_filename_id_date = sys.argv[3]
+
+    io = rf.InputOutput(input_filename, output_filename_id_zip, output_filename_id_date, '|')
+    io.read_and_process_file()
+
+
+if __name__ == "__main__":
+    main()
